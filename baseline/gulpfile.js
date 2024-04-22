cd05e5ece8f6b33f84611eaba773ac35a1915529
whois: Andy White <andy@whoisandywhite.com>
2024-04-22 16:01:40 +0100

    Update to ES6 and Gulp5

--- baseline/gulpfile.js
+++ baseline/gulpfile.js
@@ -1,16 +1,13 @@
-'use strict';
- 
-import gulp from 'gulp'; 
+import gulp from 'gulp';
 import autoprefixer from 'gulp-autoprefixer';
 import sass from 'gulp-dart-sass';
 import rename from 'gulp-rename';
 import uglifycss from 'gulp-uglifycss';
-import sourcemaps from 'gulp-sourcemaps';
-import uglify from 'gulp-uglify';
+import terser from 'gulp-terser';
 import { exec } from 'child_process';
+import path from 'path';
 
-// Assuming gulpfile.js is in the root, adjust the path to your script
-gulp.task('extract-colors', (done) => {
+function extractColors(done) {
     exec('node ./assets/build-scripts/extract-colors.js', (err, stdout, stderr) => {
         if (err) {
             console.error(`exec error: ${err}`);
@@ -18,111 +15,72 @@ gulp.task('extract-colors', (done) => {
         }
         console.log(stdout);
         console.error(stderr);
-        done(); // signals completion of the task
+        done();
     });
-});
+}
 
-gulp.task('sass', () => {
-	return gulp.src('assets/src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'compressed',
-			includePaths: ['./node_modules'],
-		}).on( 'error', sass.logError ))
-		.pipe(autoprefixer({
-			cascade: false
-		}))
-		.pipe(uglifycss({
-			'maxLineLen' : 80,
-			'uglyComments' : true
-		}))
-		.pipe(rename({
-			suffix: '.min'
-		}))
-        .pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('assets/dist/css'));
-});
+function compileSass() {
+  return gulp.src('assets/src/scss/**/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(uglifycss({ 'maxLineLen': 80, 'uglyComments': true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('assets/dist/css', { sourcemaps: '.' }));
+}
 
-gulp.task('sassBlocks', () => {
-	return gulp.src('porter/blocks/**/*.scss')
-        .pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'compressed',
-			includePaths: ['./node_modules'],
-		}).on( 'error', sass.logError ))
-		.pipe(autoprefixer({
-			cascade: false
-		}))
-		.pipe(uglifycss({
-			'maxLineLen' : 80,
-			'uglyComments' : true
-		}))
-		.pipe( rename(function (path) {
-           var temp = path.dirname.slice(0, -4);
-           path.dirname = "porter/blocks/" + temp + "css";
-        }) )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'))
-});
+function compileBlocks() {
+  return gulp.src('porter/blocks/**/scss/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(rename(function (file) {
+        file.dirname = file.dirname.replace('scss', 'css');
+        file.extname = '.css';
+    }))
+    .pipe(gulp.dest('porter/blocks/', { sourcemaps: '.' }));
+}
 
-gulp.task('sassBlockStyles', () => {
-	return gulp.src('porter/inc/block/**/*.scss')
-        .pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'compressed',
-			includePaths: ['./node_modules'],
-		}).on( 'error', sass.logError ))
-		.pipe(autoprefixer({
-			cascade: false
-		}))
-		.pipe(uglifycss({
-			'maxLineLen' : 80,
-			'uglyComments' : true
-		}))
-		.pipe( rename(function (path) {
-           var temp = path.dirname.slice(0, -4);
-           path.dirname = "porter/inc/block/" + temp + "css";
-        }) )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'))
-});
 
-gulp.task('sassVariationStyles', () => {
-	return gulp.src('porter/inc/variations/**/*.scss')
-        .pipe(sourcemaps.init())
-		.pipe(sass.sync({
-			outputStyle: 'compressed',
-			includePaths: ['./node_modules'],
-		}).on( 'error', sass.logError ))
-		.pipe(autoprefixer({
-			cascade: false
-		}))
-		.pipe(uglifycss({
-			'maxLineLen' : 80,
-			'uglyComments' : true
-		}))
-		.pipe( rename(function (path) {
-           var temp = path.dirname.slice(0, -4);
-           path.dirname = "porter/inc/variations/" + temp + "css";
-        }) )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'))
-});
+function compileBlockStyles() {
+  return gulp.src('porter/inc/block/styles/scss/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(uglifycss({ 'maxLineLen': 80, 'uglyComments': true }))
+    .pipe(rename(function (path) {
+        path.dirname = "css";
+        path.extname = '.css';
+    }))
+    .pipe(gulp.dest('porter/inc/block/styles/', { sourcemaps: '.' }));
+}
 
-gulp.task('jsMain', () => {
-    return gulp.src('assets/src/js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('assets/dist/js'));
-});
 
-gulp.task('watch', () => {
-	// css
-	gulp.watch(['theme.json'], gulp.series('extract-colors', 'sass', 'sassBlocks', 'sassBlockStyles', 'sassVariationStyles'));
-	gulp.watch('assets/src/scss/**/*.scss', gulp.series('sass'));
-	gulp.watch('porter/blocks/**/*.scss', gulp.series('sassBlocks'));
-	gulp.watch('porter/inc/block/**/*.scss', gulp.series('sassBlockStyles'));
-	gulp.watch('porter/inc/variations/**/*.scss', gulp.series('sassVariationStyles'));
-	// js
-    gulp.watch('assets/src/js/**/*.js', gulp.series('jsMain'))
-});
 
+function compileVariationStyles() {
+  return gulp.src('porter/inc/block/variations/**/scss/*.scss', { sourcemaps: true })
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(rename(function (file) {
+        file.dirname = file.dirname.replace('scss', 'css');
+        file.extname = '.css';
+    }))
+    .pipe(gulp.dest('porter/inc/block/variations/', { sourcemaps: '.' }));
+}
+
+function compileJS() {
+  return gulp.src('assets/src/js/**/*.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(gulp.dest('assets/dist/js', { sourcemaps: '.' }));
+}
+
+function watchTasks() {
+  gulp.watch('theme.json', gulp.series(extractColors, compileSass, compileBlocks, compileBlockStyles, compileVariationStyles));
+  gulp.watch('assets/src/scss/**/*.scss', compileSass);
+  gulp.watch('porter/blocks/**/scss/*.scss', compileBlocks);
+  gulp.watch('porter/inc/block/styles/scss/**/*.scss', compileBlockStyles);
+  gulp.watch('porter/inc/block/variations/**/scss/*.scss', compileVariationStyles);
+  gulp.watch('assets/src/js/**/*.js', compileJS);
+}
+
+export const build = gulp.series(extractColors, gulp.parallel(compileSass, compileBlocks, compileBlockStyles, compileVariationStyles, compileJS));
+export const watch = gulp.series(build, watchTasks);
+
+export default build;
